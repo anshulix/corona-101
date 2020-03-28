982744e5cdc5443917e1dc24191acbe8c8c1a3ba
Anshuli Gupta <>
2020-03-29 03:07:52 +0530

    title and description update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Corona-101 Tribes',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Corona is an Invisible Tsunami that is impacting everyone around us in health & wealth. The government is too far away to help effectively on the ground, so we need to take care of ourselves & our near & dear.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
